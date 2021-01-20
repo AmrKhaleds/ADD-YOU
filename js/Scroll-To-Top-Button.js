11053521e5a1e1119ed82395e9e5fd72088c6fca
AmrKhaledS <amrkhaleds898@gmail.com>
2021-01-20 22:59:59 +0200

    add scroll to top buttom

--- js/Scroll-To-Top-Button.js
+++ js/Scroll-To-Top-Button.js
@@ -2,7 +2,7 @@
 
 $(document).ready(function() {
     $(window).scroll(function() {
-    if ($(this).scrollTop() > 10) {
+    if ($(this).scrollTop() > 200) {
     $('#toTopBtn').fadeIn();
     } else {
     $('#toTopBtn').fadeOut();
